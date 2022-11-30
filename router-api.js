b0421fbc18b5abc4dac3c6ba6f5dea4553478617
itsmeberwyn <bherfelismenia@gmail.com>
2022-11-30 15:52:50 +0800

    Update

--- router-api.js
+++ router-api.js
@@ -455,11 +455,13 @@ router.post("/deleteAcc_admin", (req, res) => {
       if (response.data.status["remarks"] === "success") {
         res.redirect("/user-info");
       } else {
+        res.jsonp("response" + response.data);
         console.log(response);
         res.redirect("/user-login");
       }
     })
     .catch((error) => {
+      res.jsonp("error" + error);
       console.log(error);
       res.redirect("/user-login");
     });
